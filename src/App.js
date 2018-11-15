d02096b274e57a17b42810b6431b8aadda12602d
Elizabeth Messick <Elizabeth@MacBook-Pro-4.local>
2018-11-15 09:16:14 -0800

    create burger-builder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BurgerPane from './components/BurgerPane/BurgerPane'
+import IngredientList from './components/IngredientList/IngredientList'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ingredients: [
+        { name: 'Kaiser Bun', color: 'saddlebrown' },
+        { name: 'Sesame Bun', color: 'sandybrown' },
+        { name: 'Gluten Free Bun', color: 'peru' },
+        { name: 'Beef Patty', color: '#3F250B' },
+        { name: 'Soy Patty', color: '#3F250B' },
+        { name: 'Black Bean Patty', color: '#3F250B' },
+        { name: 'Chicken Patty', color: 'burlywood' },
+        { name: 'Lettuce', color: 'lawngreen' },
+        { name: 'Tomato', color: 'tomato' },
+        { name: 'Bacon', color: 'maroon' },
+        { name: 'Onion', color: 'lightyellow' }
+      ],
+      stack: [{ name: 'Onion', color: 'lightyellow' }]
+    }
+    this.addIngredient = this.addIngredient.bind(this)
+    this.removeIngredient = this.removeIngredient.bind(this)
+    this.clearStack = this.clearStack.bind(this)
+  }
+
+  addIngredient(ingredient) {
+    var ingred = this.state.stack;
+    ingred.unshift(ingredient)
+    this.setState({
+      ingred: ingred
+    });
+  }
+
+  removeIngredient() {
+    this.setState((state) => {
+      return { stack: state.stack.pop() }
+    });
+  }
+
+  clearStack() {
+    this.setState((state) => {
+      return { stack: state.stack = [] }
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container flex-h">
+        <IngredientList ingredients={this.state.ingredients} addIngredient={this.addIngredient} />
+        <BurgerPane stack={this.state.stack} ingredients={this.state.ingredients} removeIngredient={this.removeIngredient} clearStack={this.clearStack} />
       </div>
     );
   }
---
+++ src/components/ClearBurger/ClearBurger.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const ClearBurger = (props) => (
+    <div className='component'>
+        <button onClick={props.clearStack}>ClearBurger</button>
+    </div>
+);
+
+export default ClearBurger;
